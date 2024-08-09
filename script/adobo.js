b08f256fa551390ee97fbfc2795eb5bc8675d826
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-08-09 15:06:27 +0800

    Update adobo.js

--- script/adobo.js
+++ script/adobo.js
@@ -16,44 +16,42 @@ module.exports.run = async function({ api, event, args }) {
   const query = args.join(" ");
 
   if (!query) {
-    api.sendMessage('Please provide a query. Example: adobo what is n1gga?', event.threadID, event.messageID);
-    return;
+    return api.sendMessage('Please provide a query. Example: adobo what is n1gga?', event.threadID, event.messageID);
   }
 
-  const chillMessageInfo = await new Promise(resolve => {
-    api.sendMessage('👄 𝐀𝐃𝐎𝐁𝐎 𝐀𝐈 // answering...', event.threadID, (err, info) => {
+  const responseMessage = await new Promise((resolve, reject) => {
+    
+    api.sendMessage({
+      body: '👄 𝐀𝐃𝐎𝐁𝐎 𝐀𝐈 // answering...',
+      mentions: [{ tag: event.senderID, id: event.senderID }],
+    }, event.threadID, (err, info) => {
+      if (err) return reject(err);
       resolve(info);
-    });
+    }, event.messageID); 
   });
 
-  const apiUrl = `https://markdevs-last-api-2epw.onrender.com/api/adobo/gpt?query=${encodeURIComponent(query)}`;
-  const startTime = Date.now();
-
   try {
+    const startTime = Date.now();
+    const apiUrl = `https://markdevs-last-api-2epw.onrender.com/api/adobo/gpt?query=${encodeURIComponent(query)}`;
     const response = await axios.get(apiUrl);
     const adoboResponse = response.data.result;
     const responseTime = ((Date.now() - startTime) / 1000).toFixed(2);
-    const formattedResponseTime = `${responseTime}s`;
-
+    
     api.getUserInfo(event.senderID, async (err, userInfo) => {
       if (err) {
         console.error('Error fetching user info:', err);
-        await api.editMessage('Error fetching user info.', chillMessageInfo.messageID);
-        return;
+        return await api.editMessage('Error fetching user info.', responseMessage.messageID);
       }
 
       const userName = userInfo[event.senderID].name;
-      const formattedResponse = `👄 𝐀𝐃𝐎𝐁𝐎 𝐀𝐈 // ${formattedResponseTime}\n━━━━━━━━━━━━━━━━━━\n${adoboResponse}\n━━━━━━━━━━━━━━━━━━\n👤 𝙰𝚜𝚔𝚎𝚍 𝚋𝚢: ${userName}`;
+      const formattedResponse = `👄 𝐀𝐃𝐎𝐁𝐎 𝐀𝐈 // ${responseTime}s\n━━━━━━━━━━━━━━━━━━\n${adoboResponse}\n━━━━━━━━━━━━━━━━━━\n👤 𝙰𝚜𝚔𝚎𝚍 𝚋𝚢: ${userName}`;
 
-      try {
-        await api.editMessage(formattedResponse, chillMessageInfo.messageID);
-      } catch (error) {
-        console.error('Error editing message:', error);
-        api.sendMessage('Error editing message: ' + error.message, event.threadID, event.messageID);
-      }
+   
+      await api.editMessage(formattedResponse.trim(), responseMessage.messageID);
+
     });
   } catch (error) {
     console.error('Error:', error);
-    await api.editMessage('Error: ' + error.message, chillMessageInfo.messageID);
+    await api.editMessage('Error: ' + error.message, responseMessage.messageID);
   }
 };
