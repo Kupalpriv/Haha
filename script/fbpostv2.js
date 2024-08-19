7a52e8778a7896c61fbf1f369246de04c6d787e0
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-08-19 21:04:45 +0800

    Update fbpostv2.js

--- script/fbpostv2.js
+++ script/fbpostv2.js
@@ -56,7 +56,7 @@ module.exports.run = async function ({ api, event, args }) {
         api.sendMessage("Creating the Facebook post, please wait...", event.threadID);
 
         // Fetch the response from the Facebook Post API
-        const response = await axios.get(`https://ggwp-ifzt.onrender.com/canvas/fbpost?uid=${mentionId}&text=${encodeURIComponent(text)}&name=${encodeURIComponent(name)}`, { responseType: 'arraybuffer' });
+        const response = await axios.get(`https://deku-rest-api-3jvu.onrender.com/canvas/fbpost?uid=${mentionId}&text=${encodeURIComponent(text)}&name=${encodeURIComponent(name)}`, { responseType: 'arraybuffer' });
         const buffer = Buffer.from(response.data, 'binary');
         const filePath = path.join(__dirname, `${mentionId}.jpg`);
 
