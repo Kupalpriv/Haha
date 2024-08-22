f864437c056bc923d07853c216c40d5823b1f0cd
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-08-22 22:22:27 +0800

    Update ai2.js

--- script/ai2.js
+++ script/ai2.js
@@ -4,7 +4,7 @@ module.exports.config = {
     name: 'ai2',
     version: '1.0.0',
     role: 0,
-    hasPrefix: true,
+    hasPrefix: false,
     aliases: ['ai2'],
     description: 'Interact with the Hercai AI',
     usage: 'ai2 [question]',
