d7b234234bddd7f7f63dd543002bd261ed1451ae
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-08-24 10:05:28 +0800

    Update ashley.js

--- script/ashley.js
+++ script/ashley.js
@@ -7,42 +7,35 @@ module.exports.config = {
     credits: "chilli", 
     description: "Fetch a response from Ashley",
     hasPrefix: true,
-    aliases: ["ashkey", "ash"],
+    aliases: ["ash"],
     usage: "[ashley <query>]",
     cooldown: 5,
 };
 
 module.exports.run = async function ({ api, event, args }) {
     try {
-        // Check if a query is provided
         if (args.length === 0) {
-            api.sendMessage("Please provide a query: ex: ashley subo moto.", event.threadID);
+            api.sendMessage("Please provide a query: ex: ashley subo moto.", event.threadID, event.messageID);
             return;
         }
 
         const query = args.join(" ");
+        const initialMessage = await api.sendMessage("𝙰𝚂𝙷𝙻𝙴𝚈 𝚃𝙰𝙻𝙺𝙸𝙽𝙶....", event.threadID, event.messageID);
 
-        // Inform the user that the fetching process has started
-        const initialMessage = await api.sendMessage("𝙰𝚂𝙷𝙻𝙴𝚈 𝚃𝙰𝙻𝙺𝙸𝙽𝙶....", event.threadID);
-
-        // Fetch the response from the Ashley API
         const response = await axios.get(`https://markdevs-last-api-2epw.onrender.com/api/ashley?query=${encodeURIComponent(query)}`);
         const ashleyResponse = response.data.result;
 
-        // Check if the response contains valid data
         if (!ashleyResponse) {
-            api.sendMessage("No response found from Ashley.", event.threadID);
+            api.sendMessage("No response found from Ashley.", event.threadID, event.messageID);
             return;
         }
 
-        // Format the response message
         const formattedResponse = `🔞 𝙰𝚂𝙷𝙻𝙴𝚈 𝙷𝙾𝚁𝙽𝚈 𝙰𝙸\n━━━━━━━━━━━━━━━━━━\n${ashleyResponse}\n━━━━━━━━━━━━━━━━━━\n-𝙰𝚂𝙷𝙻𝙴𝚈 𝚃𝙸𝙶𝙽𝙰𝙽 𝙼𝙾 𝙰𝙺𝙾`;
 
-        // Edit the initial message with the formatted response
-        await api.editMessage(formattedResponse, initialMessage.messageID);
+        await api.sendMessage(formattedResponse, event.threadID, event.messageID);
 
     } catch (error) {
         console.error('Error:', error);
-        api.sendMessage("An error occurred while processing the request.", event.threadID);
+        api.sendMessage("An error occurred while processing the request.", event.threadID, event.messageID);
     }
 };
