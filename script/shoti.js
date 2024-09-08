7bc2be984cd37b1bf9ba9618c47b7f451c1827ff
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-09-08 17:14:22 +0800

    Update shoti.js

--- script/shoti.js
+++ script/shoti.js
@@ -3,48 +3,42 @@ module.exports.config = {
 	version: "1.0.0",
 	role: 0,
 	credits: "libyzxy0",
-	description: "Generate a random tiktok video.",
+	description: "Generate a random TikTok video.",
 	usages: "[]",
 	cooldown: 0,
 	hasPrefix: false,
 };
 
-module.exports. run = async ({ api, event, args }) => {
-
-	api.setMessageReaction("⏳", event.messageID, (err) => {
-		 }, true);
-api.sendTypingIndicator(event.threadID, true);
+module.exports.run = async ({ api, event, args }) => {
+	api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+	api.sendTypingIndicator(event.threadID, true);
 
 	const { messageID, threadID } = event;
 	const fs = require("fs");
 	const axios = require("axios");
 	const request = require("request");
-	const prompt = args.join(" ");
 
-	if (!prompt[0]) { api.sendMessage("Downloading...", threadID, messageID);
-		}
+	try {
+		// New API endpoint without needing an API key
+		const response = await axios.post(`https://shoti-api.adaptable.app/api/v1/request-f`);
 
- try {
-	const response = await axios.post(`https://shoti-srv1.onrender.com/api/v1/get`, { apikey: `$shoti-1hg4gifgnlfdmeslom8` });
-
-	let path = __dirname + `/cache/shoti.mp4`;
-	const file = fs.createWriteStream(path);
-	const rqs = request(encodeURI(response.data.data.url));
-	rqs.pipe(file);
-	file.on(`finish`, () => {
-		 setTimeout(function() {
-			 api.setMessageReaction("✅", event.messageID, (err) => {
-					}, true);
-			return api.sendMessage({
-			body: `𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 : @${response.data.data.user.username}\n𝖭𝗂𝖼𝗄𝗇𝖺𝗆𝖾 : ${response.data.data.user.nickname}`, 
-			attachment: fs.createReadStream(path)
-		}, threadID);
+		let path = __dirname + `/cache/shoti.mp4`;
+		const file = fs.createWriteStream(path);
+		const rqs = request(encodeURI(response.data.data.url));
+		rqs.pipe(file);
+		file.on(`finish`, () => {
+			setTimeout(function () {
+				api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+				return api.sendMessage({
+					body: `𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 : @${response.data.data.user.username}\n𝖭𝗂𝖼𝗄𝗇𝖺𝗆𝖾 : ${response.data.data.user.nickname}\n𝖳𝗂𝗍𝗅𝖾 : ${response.data.data.title}\n𝖣𝗎𝗋𝖺𝗍𝗂𝗈𝗇 : ${response.data.data.duration}`,
+					attachment: fs.createReadStream(path)
+				}, threadID);
 			}, 5000);
-				});
-	file.on(`error`, (err) => {
+		});
+		file.on(`error`, (err) => {
 			api.sendMessage(`Error: ${err}`, threadID, messageID);
-	});
-	 } catch (err) {
+		});
+	} catch (err) {
 		api.sendMessage(`Error: ${err}`, threadID, messageID);
 	};
 };
