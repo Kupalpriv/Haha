1790842b4b325dbda3491b8862bf1e7adb81f602
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-10-10 15:23:16 +0800

    Update welcomenoti.js

--- script/event/welcomenoti.js
+++ script/event/welcomenoti.js
@@ -7,7 +7,6 @@ module.exports.handleEvent = async function ({ api, event }) {
     const logMessageType = event.logMessageType;
     const threadID = event.threadID;
 
-    // Check if the event is related to a new participant being added
     if (logMessageType === "log:subscribe") {
         try {
             let { threadName, participantIDs } = await api.getThreadInfo(threadID);
@@ -16,7 +15,6 @@ module.exports.handleEvent = async function ({ api, event }) {
             for (let newParticipant of addedParticipants) {
                 let userID = newParticipant.userFbId;
 
-                // If the new participant is not the bot itself
                 if (userID !== api.getCurrentUserID()) {
                     api.shareContact(
                         `👋 Hello! Welcome to ${threadName || "this group"} 🤗, you're the ${participantIDs.length}th member on this group. Enjoy! 🤗`,
