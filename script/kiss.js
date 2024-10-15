4fbdb89ccd7028a0b2c8bdddbdba50e52f40dcfd
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-10-15 18:40:37 +0800

    Update kiss.js

--- script/kiss.js
+++ script/kiss.js
@@ -17,7 +17,6 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
     let mentionedUser;
 
-    // Check if a user is mentioned or if the command is used in reply
     if (Object.keys(event.mentions).length > 0) {
         mentionedUser = Object.keys(event.mentions)[0];
     } else if (event.messageReply && event.messageReply.senderID) {
@@ -29,27 +28,37 @@ module.exports.run = async function({ api, event, args }) {
     try {
         const apiUrl = `https://api-canvass.vercel.app/kiss2?one=${event.senderID}&two=${mentionedUser}`;
 
+        const cacheDir = path.join(__dirname, 'cache');
+        if (!fs.existsSync(cacheDir)) {
+            fs.mkdirSync(cacheDir);
+        }
+
+        const fileName = `kiss_${Date.now()}.png`;
+        const filePath = path.join(cacheDir, fileName);
         const response = await axios({
             method: 'GET',
             url: apiUrl,
             responseType: 'stream',
         });
 
-        const fileName = `kiss_${event.senderID}_${mentionedUser}.png`;
-        const filePath = path.join(__dirname, fileName);
         const writer = fs.createWriteStream(filePath);
-
         response.data.pipe(writer);
 
         writer.on('finish', async () => {
             await api.sendMessage({
                 attachment: fs.createReadStream(filePath)
             }, event.threadID, event.messageID);
-            fs.unlinkSync(filePath);  // Delete the file after sending the image
+
+            fs.unlink(filePath, (err) => {
+                if (err) console.error('Error deleting file:', err);
+            });
         });
 
         writer.on('error', () => {
             api.sendMessage('There was an error creating the kiss image. Please try again later.', event.threadID, event.messageID);
+            fs.unlink(filePath, (err) => {
+                if (err) console.error('Error deleting file:', err);
+            });
         });
     } catch (error) {
         console.error('Error fetching kiss image:', error);
