cc3a3939b7c6407529c1ce2418690e6301759c6c
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-31 10:45:22 +0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,6 @@
+// api.js
+module.exports = {
+    markApi: 'https://markdevs69v2-679r.onrender.com',
+    gptApi: 'https://betadash-api-swordslush.vercel.app/gpt-4o-mini',
+    // Add other APIs here as needed
+};
