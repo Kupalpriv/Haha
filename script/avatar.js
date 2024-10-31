84f91860a01012342e0d4536fe922ff7dc13a76e
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-31 11:50:08 +0800

    Update avatar.js

--- script/avatar.js
+++ script/avatar.js
@@ -13,6 +13,7 @@ module.exports.config = {
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
+const { josh } = require('../api'); 
 
 module.exports.run = async function({ api, event, args }) {
     try {
@@ -23,7 +24,7 @@ module.exports.run = async function({ api, event, args }) {
             return api.sendMessage("Please provide all required parameters: id-1to800 | bgtext | signature | color.", event.threadID);
         }
 
-        const apiUrl = `https://joshweb.click/canvas/avatarv2?id=${encodeURIComponent(id)}&bgtext=${encodeURIComponent(bgtext)}&signature=${encodeURIComponent(signature)}&color=${encodeURIComponent(color)}`;
+        const apiUrl = `${josh}canvas/avatarv2?id=${encodeURIComponent(id)}&bgtext=${encodeURIComponent(bgtext)}&signature=${encodeURIComponent(signature)}&color=${encodeURIComponent(color)}`;
 
         api.sendMessage("Generating avatar, please wait...", event.threadID);
 
