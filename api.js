d7b7c17fc3d785b16d81a500bf0d07e332631005
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-31 17:17:02 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // api.js
 module.exports = {
-    markApi: 'https://markdevs69v2-679r.onrender.com',
+    markApi: 'https://markdevs-last-api-vtjp.onrender.com',
     josh: 'https://joshweb.click',
     canvas: 'https://api-canvass.vercel.app',
     jonel: 'https://appjonellccapis.zapto.org',   
