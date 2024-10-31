4a286f8491e085b0176a651c24bb832d624ebdeb
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-31 11:58:43 +0800

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,7 @@
 module.exports = {
     markApi: 'https://markdevs69v2-679r.onrender.com',
     josh: 'https://joshweb.click',
+    canvas: 'https://api-canvass.vercel.app',
     gptApi: 'https://betadash-api-swordslush.vercel.app/gpt-4o-mini',
     // Add other APIs here as needed
 };
