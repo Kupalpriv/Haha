71dfa0ab516d834754c38fd570b115270b9e9e20
Kupalpriv <churchillabing100@gmail.com>
2024-11-13 20:24:16 +0800

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,8 @@
 module.exports = {
     markApi: 'https://markdevs-last-api-vtjp.onrender.com',
     josh: 'https://joshweb.click',
+    neth: 'https://nethwieginedev.vercel.app',
+    kenlie: 'https://api.kenliejugarap.com',
     canvas: 'https://api-canvass.vercel.app',
     jonel: 'https://appjonellccapis.zapto.org',   
 };
