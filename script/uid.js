fe43ad3a01b25b366e4e5550cf18cbf97129c9c5
Kupalpriv <churchillabing100@gmail.com>
2024-12-02 19:13:14 +0800

    Update uid.js

--- script/uid.js
+++ script/uid.js
@@ -7,68 +7,67 @@ async function findUid(link) {
     const response = await axios.post(
       'https://seomagnifier.com/fbid',
       new URLSearchParams({
-        'facebook': '1',
-        'sitelink': link
+        facebook: '1',
+        sitelink: link,
       }),
       {
         headers: {
-          'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-          'Cookie': 'PHPSESSID=0d8feddd151431cf35ccb0522b056dc6'
-        }
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          Cookie: 'PHPSESSID=0d8feddd151431cf35ccb0522b056dc6',
+        },
       }
     );
+
     const id = response.data;
-    if (isNaN(id)) {
-      const html = await axios.get(link);
-      const $ = cheerio.load(html.data);
-      const el = $('meta[property="al:android:url"]').attr('content');
-      if (!el) {
-        throw new Error('UID not found');
-      }
-      const number = el.split('/').pop();
-      return number;
+    if (!isNaN(id)) {
+      return id;
     }
-    return id;
+
+    const html = await axios.get(link);
+    const $ = cheerio.load(html.data);
+    const metaContent = $('meta[property="al:android:url"]').attr('content');
+    if (metaContent) {
+      const uid = metaContent.split('/').pop();
+      return uid;
+    }
+
+    throw new Error('UID not found.');
   } catch (error) {
-    throw new Error('An unexpected error occurred. Please try again.');
+    throw new Error('An error occurred while fetching UID.');
   }
 }
 
 module.exports.config = {
-  name: "uid",
+  name: 'uid',
   role: 0,
-  credits: "Cliff",
-  description: "Get the user's Facebook UID.",
-  hasPrefix: false,
-  usages: "{p}uid {p}uid @mention {p}uid fblink",
+  credits: 'Churchill',
+  description: 'Get a user’s Facebook UID.',
+  hasPrefix: true,
+  usages: '{p}uid | {p}uid @mention | {p}uid <fblink>',
   cooldown: 5,
   aliases: [],
 };
 
-module.exports.run = async function({ api, event, args }) {
-  if (Object.keys(event.mentions).length === 0) {
-    if (event.messageReply) {
-      const senderID = event.messageReply.senderID;
-      return api.shareContact(senderID, event.messageReply.senderID, event.threadID);
-    } else {
-      return api.shareContact(event.senderID, event.senderID, event.threadID);
-    }
-  } else {
+module.exports.run = async function ({ api, event, args }) {
+  if (!args[0] && Object.keys(event.mentions).length === 0) {
+    const senderID = event.messageReply?.senderID || event.senderID;
+    return api.shareContact(senderID, senderID, event.threadID);
+  }
+
+  if (Object.keys(event.mentions).length > 0) {
     for (const mentionID in event.mentions) {
-      const { mentions } = event;
-      for (const id in mentions) {
-        api.shareContact(`${mentions[id].replace("@", "")}: ${mentionID}`, mentionID, event.threadID);
-      }
+      await api.shareContact(mentionID, mentionID, event.threadID);
     }
-if (args[0].match(regExCheckURL)) {
-      let msg = '';
-      for (const link of args) {
-        try {
-          const uid = await findUid(link);
-          await api.sendMessage(`${link}: ${uid}`, uid, event.threadID);
-        } catch (error) {
-          await api.sendMessage(`Error fetching UID for ${link}`, event.threadID);
-        }
+    return;
+  }
+
+  if (args[0]?.match(regExCheckURL)) {
+    for (const link of args) {
+      try {
+        const uid = await findUid(link);
+        await api.shareContact(uid, uid, event.threadID);
+      } catch {
+        await api.sendMessage(`❌ Unable to fetch UID for ${link}.`, event.threadID, event.messageID);
       }
     }
   }
